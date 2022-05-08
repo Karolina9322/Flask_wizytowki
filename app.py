6ada414e5ea8ef3efdd1205fe0535415ea574120
Karolina Krzęcio <karolina.krzecio@gmail.com>
2022-05-08 14:23:10 +0200

    Poprawki

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ def hello():
 
 @app.route('/mypage/me')
 def mypage_me():
-    return render_template("Moja_strona_1.html")
+    return render_template("moja_strona_1.html")
 
 
 @app.route("/mypage/contact", methods=['GET', 'POST'])
 def mypage_contact():
     if request.method == 'GET':
         print("We received GET")
-        return render_template("Moja_strona_2.html")
+        return render_template("moja_strona_2.html")
     elif request.method == 'POST':
        print("We received POST")
        print(request.form)
